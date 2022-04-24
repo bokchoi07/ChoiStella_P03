7dda7d50ac9a69697137db59f09b8e2b4c25ea0b
Stella Choi <stellachoi120701@gmail.com>
2022-04-24 10:49:44 -0500

    materials, scripts, camera

---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour
+{
+    private void Start()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed = 12f;
+
+    public GameObject playerGameObject;
+    private void Start()
+    {
+        playerGameObject = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            
+        }
+    }
+}
